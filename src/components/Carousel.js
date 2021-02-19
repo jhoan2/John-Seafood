f2b1d3e520113431de977d0befcc3d41574bf1d3
John Hoang <67968051+jhoan2@users.noreply.github.com>
2021-02-19 16:43:22 -0600

    carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -3,19 +3,19 @@ import { Carousel } from 'react-bootstrap';
 import twofish from '../images/twofish.jpg';
 import combo from '../images/combination order.jpg';
 import oyster from '../images/oyster.jpg';
-
+import './carousel.css'
 export default function carousel() {
     return (
         <div>
             <div>
-                <Carousel className='h-50' style={{marginBottom: 50}}>
+                <Carousel style={{marginBottom: 50}}>
                     <Carousel.Item interval={3000}>
                         <img 
                             className="d-block w-100"
                             src={twofish}
                             alt="first slide"
                         />
-                        <Carousel.Caption>
+                        <Carousel.Caption className='text-dark'>
                             <h3>2 Fish</h3>
                         </Carousel.Caption>
                     </Carousel.Item>
@@ -25,7 +25,7 @@ export default function carousel() {
                             src={combo}
                             alt="second slide"
                         />
-                        <Carousel.Caption>
+                        <Carousel.Caption className='text-dark'>
                             <h3>Popcorn Shrimp 4 Fish Strips</h3>
                         </Carousel.Caption>
                     </Carousel.Item>
@@ -35,7 +35,7 @@ export default function carousel() {
                             src={oyster}
                             alt="third slide"
                         />
-                        <Carousel.Caption>
+                        <Carousel.Caption className='text-dark'>
                             <h3>6 Oysters</h3>
                         </Carousel.Caption>
                     </Carousel.Item>
