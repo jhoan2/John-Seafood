822cc91408d9fe5f1daf3966ec36c9870b160a8a
John Hoang <67968051+jhoan2@users.noreply.github.com>
2021-03-01 22:25:27 -0600

    changing size for react icons

--- src/components/cart/CartItem.js
+++ src/components/cart/CartItem.js
@@ -1,14 +1,17 @@
 import React from 'react'
-import { ListGroup, Button } from 'react-bootstrap'
+import { ListGroup } from 'react-bootstrap'
+import { AiFillMinusCircle, AiFillPlusCircle } from 'react-icons/ai';
 
 export default function CartItem({title, amount, price}) {
     return (
-        <ListGroup.Item className="d-flex justify-content-between">
+        <ListGroup.Item className="d-flex justify-content-between align-items-center p-6">
             <h6>{title}</h6>
-            <p>
-                <Button size='sm'> + </Button>
+            <div>
+                <AiFillPlusCircle size={30} />
                     ({amount})
-                <Button size='sm'> - </Button>
+                <AiFillMinusCircle size={30} />
+            </div>
+            <p>
                 <b>$</b>{price}
             </p>
         </ListGroup.Item>
