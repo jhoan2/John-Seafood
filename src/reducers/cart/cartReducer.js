42ccaa6393c9caab8c5cb872a3db006dfa811a1c
John Hoang <67968051+jhoan2@users.noreply.github.com>
2021-03-04 15:53:30 -0600

    decrease reducer, total per item in cart

--- src/reducers/cart/cartReducer.js
+++ src/reducers/cart/cartReducer.js
@@ -3,7 +3,7 @@ import products from '../../products';
 
 const INITIAL_STATE = { 
   products,
-  cart: []
+  cart: [],
 }
 
 const cartReducer = (state=INITIAL_STATE, action) => {
@@ -13,10 +13,22 @@ const cartReducer = (state=INITIAL_STATE, action) => {
       const inCart = state.cart.find((item) => item.id === action.payload.id) ? true : false
       return {
         ...state,
-        cart: inCart ? state.cart.map((item) => item.id === action.payload.id ? { ...item, amount: item.amount + 1 } : item) : [ ...state.cart, {...item, amount: 1}],
+        cart: inCart ? state.cart.map((item) => item.id === action.payload.id ? { ...item, amount: item.amount + 1 } : item) : [ ...state.cart, {...item, amount: 1}],  
             }
     case REMOVE_FROM_CART:
-      return {}
+      let tempCart = []
+      if (action.payload.amount === 1) {
+        tempCart = state.cart.filter((item) => item.id !== action.payload.id)
+      }
+      else {
+        tempCart = state.cart.map((item) => {
+          if (item.id === action.payload.id) {
+            return {...item, amount: item.amount - 1}
+          }
+          return item
+        })
+      }
+      return { ...state, cart: tempCart}
     case GET_TOTAL:
       return {}
     default: 
