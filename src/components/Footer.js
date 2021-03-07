3ad5dc97510a7f1aa3de89bb399c6c7ea17fc145
John Hoang <67968051+jhoan2@users.noreply.github.com>
2021-03-07 00:08:52 -0600

    fixed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,12 +14,8 @@ export default function Footer() {
     <div className='footer'>
         <Container fluid className='text-center bg-light'>
           <Row className='pt-3'>
-            <Col><h5>Hours</h5></Col>
-            <Col><h5>Phone Number</h5></Col>
-            <Col><h5>Address</h5></Col>
-          </Row>
-          <Row>
-            <Col>
+            <Col xs={12} md={4}>
+              <h5>Hours</h5>
               <ul className='list-unstyled'>
                 <li >M-T: 10:00am - 9:30pm</li>
                 <li>Fri: 10:00am - 10:30pm</li>
@@ -27,8 +23,14 @@ export default function Footer() {
                 <li>Sun 11:00am - 7:00pm</li>
               </ul> 
             </Col>
-            <Col>(225) 357-5958</Col>
-            <Col>2750 Scenic Hwy. Baton Rouge, LA 70805</Col>
+            <Col xs={12} md={4}>
+              <h5>Phone Number</h5>
+              <p>(225) 357-5958</p>
+            </Col>
+            <Col xs={12} md={4}>
+              <h5>Address</h5>
+              <p>2750 Scenic Hwy. Baton Rouge, LA 70805</p>
+            </Col>
           </Row>
           <Row className="justify-content-center">
             <p className='px-2'>
